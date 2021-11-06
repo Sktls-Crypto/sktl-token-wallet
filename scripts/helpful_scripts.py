125314775f9e10b5a30ad9251f05d653a3214f92
Daniel Ng <danielng56@gmail.com>
2021-11-06 13:05:53 -0400

    fixed the unittests

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -3,9 +3,9 @@ from brownie import (
     network,
     config,
     Contract,
-    MockV3Aggregator,
-    MockDAI,
-    MockWETH,
+    # MockV3Aggregator,
+    # MockDAI,
+    # MockWETH,
 )
 import eth_utils
 
@@ -15,10 +15,10 @@ INITIAL_PRICE_FEED_VALUE = 2000000000000000000000
 DECIMALS = 18
 
 contract_to_mock = {
-    "eth_usd_price_feed": MockV3Aggregator,
-    "dai_usd_price_feed": MockV3Aggregator,
-    "fau_token": MockDAI,
-    "weth_token": MockWETH,
+    # "eth_usd_price_feed": MockV3Aggregator,
+    # "dai_usd_price_feed": MockV3Aggregator,
+    # "fau_token": MockDAI,
+    # "weth_token": MockWETH,
 }
 
 
@@ -131,14 +131,14 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_PRICE_FEED_VALUE):
     print("Deploying Mocks...")
     account = get_account()
     print("Deploying Mock Price Feed...")
-    mock_price_feed = MockV3Aggregator.deploy(
-        decimals, initial_value, {"from": account}
-    )
-    print(f"Deployed to {mock_price_feed.address}")
-    print("Deploying Mock DAI...")
-    mock_dai = MockDAI.deploy({"from": account})
-    print(f"Deployed to {mock_dai.address}")
-    print("Deploying Mock WETH...")
-    mock_weth = MockWETH.deploy({"from": account})
-    print(f"Deployed to {mock_weth.address}")
-    print("Mocks Deployed!")
+    # mock_price_feed = MockV3Aggregator.deploy(
+    #     decimals, initial_value, {"from": account}
+    # )
+    # print(f"Deployed to {mock_price_feed.address}")
+    # print("Deploying Mock DAI...")
+    # mock_dai = MockDAI.deploy({"from": account})
+    # print(f"Deployed to {mock_dai.address}")
+    # print("Deploying Mock WETH...")
+    # mock_weth = MockWETH.deploy({"from": account})
+    # print(f"Deployed to {mock_weth.address}")
+    # print("Mocks Deployed!")
