4f1f68d2f33f7df8db1424aafe0cb7a747931af5
Daniel Ng <danielng56@gmail.com>
2021-12-22 10:27:03 -0500

    successfully airdropped

--- scripts/deploy_sktl.py
+++ scripts/deploy_sktl.py
@@ -1,4 +1,4 @@
-from brownie import SKTL, network, config
+from brownie import SKTL, SktlAirdrop, network, config
 from scripts.helpful_scripts import get_account, get_contract
 import shutil
 import os
@@ -8,8 +8,11 @@ from web3 import Web3
 import pandas as pd
 import logbook
 import sys
+from math import floor
+from brownie.network.gas.strategies import LinearScalingStrategy
+from brownie.network import gas_price
 
-logger = logbook.Logger('deploy_sktl')
+logger = logbook.Logger("deploy_sktl")
 logbook.StreamHandler(sys.stdout).push_application()
 
 # KEPT_BALANCE = Web3.toWei(100, "ether")
@@ -22,8 +25,8 @@ def deploy_sktls(update_front_end=False):
         # copy_front_end()
         pass
 
-    logger.info(f'{sktl_token=}')
-    logger.info(f'{sktl_token.totalSupply()=}')
+    logger.info(f"{sktl_token=}")
+    logger.info(f"{sktl_token.totalSupply()=}")
 
 
 def get_total_supply():
@@ -31,19 +34,52 @@ def get_total_supply():
     logger.info(f"total supply = {SKTL[0].totalSupply({'from': account}) / 10 ** 18}")
     logger.info(f"owner account = {SKTL[0].balanceOf(account) / 10 ** 18}")
 
+
+def deploy_airdrop():
+    account = get_account()
+    sktlairdrop = SktlAirdrop.deploy(SKTL[0], {"from": account}, publish_source=True)
+    logger.info(f"Deploy success to {sktlairdrop.address}")
+
+
+def deploy_approve():
+    SKTL[0].approve(SktlAirdrop[0], SKTL[0].balanceOf(get_account(0)), {"from": get_account(0)})
+
+
+def airdrop():
+    address_file = "airdrop_address_20211222.json"
+    with open(address_file, "r") as afile:
+        addr = json.loads(afile.read())
+
+    validated_addr = list(
+        set(
+            [a for a in addr if (isinstance(a, str) and len(a) == 42 and a[:2] == "0x")]
+        )
+    )
+
+    amt = floor((100 * 10 ** 6 * 10 ** 18) / len(validated_addr))
+    amt = 100000 * (10 ** 18)
+    logger.info(f"Going to drop {len(validated_addr)} addresses for {amt} sktls")
+    # print(validated_addr)
+    # gas_strategy = LinearScalingStrategy("10 gwei", "100000 gwei", 1.1)
+    # gas_price(gas_strategy)
+    # network.gas_limit(10 ** 17)
+
+    SktlAirdrop[0].airDrop(validated_addr, amt, {"from": get_account(0)})
+
+
 # def add_reward():
 #     account = get_account()
 #     sktl = Sktl20.at('0x292135fF911E6081ecC90F3bD1f9CDaAED5C78cD')
 #     sktl.rewards(5300 * (10 ** 18), {"from": account})
-# 
+#
 # def copy_front_end():
 #     print("Updating front end...")
 #     # The Build
 #     copy_folders_to_front_end("./build/contracts", "./front_end/src/chain-info")
-# 
+#
 #     # The Contracts
 #     copy_folders_to_front_end("./contracts", "./front_end/src/contracts")
-# 
+#
 #     # The ERC20
 #     copy_files_to_front_end(
 #         "./build/contracts/dependencies/OpenZeppelin/openzeppelin-contracts@4.3.2/ERC20.json",
@@ -53,7 +89,7 @@ def get_total_supply():
 #     copy_files_to_front_end(
 #         "./build/deployments/map.json", "./front_end/src/chain-info/map.json",
 #     )
-# 
+#
 #     # The Config, converted from YAML to JSON
 #     with open("brownie-config.yaml", "r") as brownie_config:
 #         config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
@@ -62,25 +98,26 @@ def get_total_supply():
 #         ) as brownie_config_json:
 #             json.dump(config_dict, brownie_config_json)
 #     print("Front end updated!")
-# 
-# 
+#
+#
 # def copy_folders_to_front_end(src, dest):
 #     if os.path.exists(dest):
 #         shutil.rmtree(dest)
 #     shutil.copytree(src, dest)
-# 
-# 
+#
+#
 # def copy_files_to_front_end(src, dest):
 #     if os.path.exists(dest):
 #         os.remove(dest)
 #     shutil.copyfile(src, dest)
-# 
-# 
+#
+#
 # def air_drop():
 #     df = pd.read_csv('/Users/danielng/projects/sktls-defi/temp/sktl_init_drop_address.csv')
 #     for addr in df.address:
 #         Sktl20[-1].transfer(addr, 1000 * (10 ** 18), {"from": get_account(0)})
-# 
+#
+
 
 def main():
     deploy_sktls(update_front_end=False)
