98288a0cd02ee35437c26dbe1c1dd436a54decd0
Daniel Ng <danielng56@gmail.com>
2021-12-30 11:11:48 -0500

    refractor

--- tests/test_sktl_long.py
+++ tests/test_sktl_long.py
@@ -1,5 +1,4 @@
 import unittest
-import brownie
 from brownie import SKTL
 import random
 from scripts.helpful_scripts import get_account
@@ -41,7 +40,7 @@ class TestSKTLLongTests(unittest.TestCase):
         self.token = SKTL.deploy({"from": get_account(0)})
 
         for i in range(1, len(self.init_acc_tokens)):
-            if (self.init_acc_tokens[i] > 0):
+            if self.init_acc_tokens[i] > 0:
                 self.token.transfer(get_account(i), self.init_acc_tokens[i] * DECIMALS)
 
     def print_current_status(self, transfered_record, reward_record):
@@ -50,76 +49,78 @@ class TestSKTLLongTests(unittest.TestCase):
                 f"{i}: bal:{self.token.balanceOf(get_account(i))}, tran={transfered_record[i]}, rew_bal:{self.token.rewardBalance(get_account(i))}, rew_rec:{reward_record[i]}"
             )
 
+    def _test_transfer(self, transfered_record, reward_record, loop):
+        # TEST transfer
+        fromacc = random.randint(1, 9)
+        toacc = random.randint(1, 9)
+
+        while toacc == fromacc:
+            toacc = random.randint(1, 9)
+
+        if self.token.balanceOf(get_account(fromacc)) == 0:
+            return
+
+        transfer_pct = random.random()
+        transfer_amt = int(self.token.balanceOf(get_account(fromacc)) * transfer_pct)
+        transfered_record[fromacc] -= transfer_amt
+        transfered_record[toacc] += transfer_amt
+        print()
+        print(f"***Test Transfer {loop=} {fromacc=} {toacc=} {transfer_amt=}***")
+
+        self.token.transfer(
+            get_account(toacc), transfer_amt, {"from": get_account(fromacc)}
+        )
+
+        for i in (fromacc, toacc):
+            self.assertIntAlmostEqual(
+                self.token.balanceOf(get_account(i)),
+                self.init_acc_tokens[i] * DECIMALS
+                + transfered_record[i]
+                + reward_record[i],
+                f"{loop=}: acount[{i}] balance not match after transfer",
+            )
+        print("==After Transfer==")
+        self.print_current_status(transfered_record, reward_record)
+
+    def _test_reward(self, transfered_record, reward_record, loop):
+        # TEST Reward
+        reward_amt = int(1000000 * random.random() * DECIMALS)
+
+        print()
+        print(f"***Test reward {loop=} {reward_amt=} {self.token.totalSupply()=}***")
+
+        for i in range(10):
+            tot_balance = self.token.balanceOf(
+                get_account(i)
+            ) + self.token.rewardBalance(get_account(i))
+            reward_record[i] += int(reward_amt * tot_balance / self.token.totalSupply())
+        self.token.increaseReward(reward_amt, {"from": get_account(0)})
+        print("==After Reward==")
+        self.print_current_status(transfered_record, reward_record)
+
+        # skip 1, because it's the owner
+        for i in range(1, 10):
+            tot_balance = self.token.balanceOf(
+                get_account(i)
+            ) + self.token.rewardBalance(get_account(i))
+            if tot_balance == 0:
+                continue
+            self.assertIntAlmostEqual(
+                self.token.balanceOf(get_account(i))
+                + self.token.rewardBalance(get_account(i)),
+                self.init_acc_tokens[i] * DECIMALS
+                + transfered_record[i]
+                + reward_record[i],
+                f"{loop=}: acount[{i}] balance not match after reward",
+            )
+
     def test_random_transfer_and_reward(self):
-        random.seed('a')
+        # to fix the random seed, for debug only
+        # random.seed("a")
         self.init_acc_tokens[0] = 100 * 10 ** 6 - sum(self.init_acc_tokens[1:])
         transfered_record = defaultdict(int)  # {account => amt}
         reward_record = defaultdict(int)  # {account => amt}
 
         for loop in range(20):
-            # TEST transfer
-            fromacc = random.randint(1, 9)
-            toacc = random.randint(1, 9)
-
-            while toacc == fromacc:
-                toacc = random.randint(1, 9)
-
-            if self.token.balanceOf(get_account(fromacc)) == 0:
-                continue
-
-            transfer_pct = random.random()
-            transfer_amt = int(
-                self.token.balanceOf(get_account(fromacc)) * transfer_pct
-            )
-            transfered_record[fromacc] -= transfer_amt
-            transfered_record[toacc] += transfer_amt
-            print()
-            print(f"***Test Transfer {loop=} {fromacc=} {toacc=} {transfer_amt=}***")
-
-            self.token.transfer(
-                get_account(toacc), transfer_amt, {"from": get_account(fromacc)}
-            )
-
-            for i in (fromacc, toacc):
-                self.assertIntAlmostEqual(
-                    self.token.balanceOf(get_account(i)),
-                    self.init_acc_tokens[i] * DECIMALS
-                    + transfered_record[i]
-                    + reward_record[i],
-                    f"{loop=}: acount[{i}] balance not match after transfer",
-                )
-            print('==After Transfer==')
-            self.print_current_status(transfered_record, reward_record)
-
-            # TEST Reward
-            reward_amt = int(1000000 * random.random() * DECIMALS)
-
-            print()
-            print(f"***Test reward {loop=} {reward_amt=} {self.token.totalSupply()=}***")
-
-            for i in range(10):
-                tot_balance = self.token.balanceOf(
-                    get_account(i)
-                ) + self.token.rewardBalance(get_account(i))
-                reward_record[i] += int(
-                    reward_amt * tot_balance / self.token.totalSupply()
-                )
-            self.token.increaseReward(reward_amt, {"from": get_account(0)})
-            print('==After Reward==')
-            self.print_current_status(transfered_record, reward_record)
-
-            # skip 1, because it's the owner
-            for i in range(1, 10):
-                tot_balance = self.token.balanceOf(
-                    get_account(i)
-                ) + self.token.rewardBalance(get_account(i))
-                if tot_balance == 0:
-                    continue
-                self.assertIntAlmostEqual(
-                    self.token.balanceOf(get_account(i))
-                    + self.token.rewardBalance(get_account(i)),
-                    self.init_acc_tokens[i] * DECIMALS
-                    + transfered_record[i]
-                    + reward_record[i],
-                    f"{loop=}: acount[{i}] balance not match after reward",
-                )
+            self._test_transfer(transfered_record, reward_record, loop)
+            self._test_reward(transfered_record, reward_record, loop)
