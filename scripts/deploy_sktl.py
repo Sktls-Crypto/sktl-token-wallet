c95f2cf3bf0ef385ed0587f2a28d6ec4d0ccb5d8
Daniel Ng <danielng56@gmail.com>
2022-05-01 19:38:04 -0400

    updated front end

--- scripts/deploy_sktl.py
+++ scripts/deploy_sktl.py
@@ -98,46 +98,47 @@ def airdrop():
 #     sktl = Sktl20.at('0x292135fF911E6081ecC90F3bD1f9CDaAED5C78cD')
 #     sktl.rewards(5300 * (10 ** 18), {"from": account})
 #
-# def copy_front_end():
-#     print("Updating front end...")
-#     # The Build
-#     copy_folders_to_front_end("./build/contracts", "./front_end/src/chain-info")
-#
-#     # The Contracts
-#     copy_folders_to_front_end("./contracts", "./front_end/src/contracts")
-#
-#     # The ERC20
-#     copy_files_to_front_end(
-#         "./build/contracts/dependencies/OpenZeppelin/openzeppelin-contracts@4.3.2/ERC20.json",
-#         "./front_end/src/chain-info/ERC20.json",
-#     )
-#     # The Map
-#     copy_files_to_front_end(
-#         "./build/deployments/map.json", "./front_end/src/chain-info/map.json",
-#     )
-#
-#     # The Config, converted from YAML to JSON
-#     with open("brownie-config.yaml", "r") as brownie_config:
-#         config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
-#         with open(
-#             "./front_end/src/brownie-config-json.json", "w"
-#         ) as brownie_config_json:
-#             json.dump(config_dict, brownie_config_json)
-#     print("Front end updated!")
-#
-#
-# def copy_folders_to_front_end(src, dest):
-#     if os.path.exists(dest):
-#         shutil.rmtree(dest)
-#     shutil.copytree(src, dest)
-#
-#
-# def copy_files_to_front_end(src, dest):
-#     if os.path.exists(dest):
-#         os.remove(dest)
-#     shutil.copyfile(src, dest)
-#
-#
+def copy_front_end():
+    print("Updating front end...")
+    # The Build
+    copy_folders_to_front_end("./build/contracts", "./front_end/src/chain-info")
+
+    # The Contracts
+    copy_folders_to_front_end("./contracts", "./front_end/src/contracts")
+
+    # The ERC20
+    copy_folders_to_front_end(
+        "./build/contracts/dependencies/OpenZeppelin/openzeppelin-contracts@4.6.0",
+        "./front_end/src/chain-info",
+        False,
+    )
+    # The Map
+    copy_files_to_front_end(
+        "./build/deployments/map.json", "./front_end/src/chain-info/map.json",
+    )
+
+    # The Config, converted from YAML to JSON
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_dict = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open(
+            "./front_end/src/brownie-config-json.json", "w"
+        ) as brownie_config_json:
+            json.dump(config_dict, brownie_config_json)
+    print("Front end updated!")
+
+
+def copy_folders_to_front_end(src, dest, cleanup=True):
+    if cleanup and os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest, dirs_exist_ok=not cleanup)
+
+
+def copy_files_to_front_end(src, dest):
+    if os.path.exists(dest):
+        os.remove(dest)
+    shutil.copyfile(src, dest)
+
+
 # def air_drop():
 #     df = pd.read_csv('/Users/danielng/projects/sktls-defi/temp/sktl_init_drop_address.csv')
 #     for addr in df.address:
